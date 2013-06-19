5cc9446ab38a19e0ee4da446160698dd2e4d7cae
Matt Jeanes <mattjeanes23@gmail.com>
2013-06-19 22:15:13 +0100

    Enabled crosshair
    This is a nice simple improvement.

--- lua/weapons/swep_sonicsd/cl_init.lua
+++ lua/weapons/swep_sonicsd/cl_init.lua
@@ -4,4 +4,4 @@ SWEP.PrintName          = "Sonic Screwdriver"
 SWEP.Slot               = 2
 SWEP.SlotPos            = 1
 SWEP.DrawAmmo           = false
-SWEP.DrawCrosshair      = false
+SWEP.DrawCrosshair      = true
