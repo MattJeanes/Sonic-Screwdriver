34545884bec2300f176d36440ea017f06b7fb344
Artiom Parygin <artiom.par@ya.ru>
2021-11-23 20:55:29 +0300

    First attempt (doesn't work)

--- lua/sonicsd/sh_sonics.lua
+++ lua/sonicsd/sh_sonics.lua
@@ -9,6 +9,51 @@ function SonicSD:AddSonic(t)
 	else
 		self.sonics[t.ID]=t
 	end
+
+	local weap = {}
+
+	weap.Category = "Doctor Who"
+	weap.PrintName = t.Name
+	if file.Exists("materials/vgui/weapons/sonic/"..t.ID..".vtf", "GAME")
+	then
+		weap.IconOverride="vgui/weapons/sonic/"..t.ID..".vtf"
+	else
+		weap.IconOverride="vgui/weapons/sonic/"..t.ID..".png"
+	end
+	weap.ScriptedEntityType = "sonic_screwdriver"
+	list.Set("Weapon", t.ID, weap)
+	print("ADDED SONIC SWEP "..t.ID)
+
 end
+
+
+hook.Add("PostGamemodeLoaded", "sonic-skins", function()
+	if not spawnmenu then return end
+	spawnmenu.AddContentType("sonic_screwdriver", function(container, obj)
+		--if not obj.material then return end
+		--if not obj.nicename then return end
+		--if not obj.spawnname then return end
+		print("ADDED CONTENT TYPE")
+
+		local icon = vgui.Create("ContentIcon", container)
+		icon:SetContentType("weapon")
+		--icon:SetSpawnName(obj.spawnname)
+		icon:SetName(obj.nicename)
+		icon:SetMaterial(obj.material)
+		icon:SetAdminOnly(obj.admin)
+		icon:SetColor(Color(205, 92, 92, 255))
+		icon.DoClick = function()
+			--RunConsoleCommand("tardis2_spawn", obj.spawnname)
+			surface.PlaySound("ui/buttonclickrelease.wav")
+		end
+
+		if IsValid(container) then
+			container:Add(icon)
+		end
+
+		return icon
+	end)
+end)
+
 
 SonicSD:LoadFolder("sonics",false,true)
