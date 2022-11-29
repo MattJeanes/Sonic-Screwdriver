cf6d0a24ae3e14102fea9d919053a8272c99d952
Artiom Parygin <artiom.par@ya.ru>
2022-11-29 15:59:19 +0300

    Sonic light turning on and off

--- lua/matproxy/sonicsd_color.lua
+++ lua/matproxy/sonicsd_color.lua
@@ -19,9 +19,17 @@ matproxy.Add(
         local col = Vector(GetConVarNumber("sonic_light_r")/255, GetConVarNumber("sonic_light_g")/255, GetConVarNumber("sonic_light_b")/255)
         if not isvector( col ) then return end
 
-        local mul = (1 + math.sin( CurTime() * 5 ) ) * 0.5
+        local keydown1=owner:KeyDown(IN_ATTACK)
+        local keydown2=owner:KeyDown(IN_ATTACK2)
 
-        mat:SetVector( self.ResultTo, col + col * mul );
+        local mul
+        if keydown1 or keydown2 then
+            mul = 1.5 + math.sin( CurTime() * 5 ) * 0.75
+        else
+            mul = 0.05
+        end
+
+        mat:SetVector( self.ResultTo, col * mul );
 
     end
 })
