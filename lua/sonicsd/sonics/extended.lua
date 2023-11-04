3bddc680894d39ef5b4e10a95cd657cf61337de4
PoogieTheGoose <andreupsgusarov@gmail.com>
2023-11-04 15:12:30 +0300

    Different sound on right click

--- lua/sonicsd/sonics/extended.lua
+++ lua/sonicsd/sonics/extended.lua
@@ -5,7 +5,9 @@ SonicSD:AddSonic({
     WorldModel = "models/doctor_who/sonic_screwdriver/w_10thsonicsd.mdl",
     LightPos = Vector(20,-2.5,-3.15),
     LightBrightness = 2,
-    SoundLoop = "sonicsd/loop_2005.wav",
+    SoundLoop = "sonicsd/loop_2005_11111.wav",
+    SoundLoop2 = "sonicsd/loop_2005_2.wav",
+    ButtonSoundOff = "sonicsd/button_off_2.wav",
     DefaultLightColor = Color(0, 180, 200),
     DefaultLightColor2 = Color(30, 200, 250)
 })
@@ -15,6 +17,7 @@ SonicSD:AddSonic({
     Name = "1968 Screwdriver",
     ViewModel = "models/doctor_who/sonic_screwdriver/c_4thsonicsd.mdl",
     WorldModel = "models/doctor_who/sonic_screwdriver/w_4thsonicsd.mdl",
-    SoundLoop = "sonicsd/loop_1963.wav",
+    SoundLoop = "sonicsd/loop_1968_1.wav",
+    SoundLoop2 = "sonicsd/loop_1968_2.wav",
     LightDisabled = true
 })
--- lua/weapons/swep_sonicsd/modules/cl_sound.lua
+++ lua/weapons/swep_sonicsd/modules/cl_sound.lua
@@ -3,14 +3,16 @@
 SWEP:AddHook("Initialize", "sound", function(self)
     self.curbeep=0
     self.eyeangles=Angle(0,0,0)
-    self.sound=CreateSound(self,self:GetSonic().SoundLoop)
+    self.sound1=CreateSound(self,self:GetSonic().SoundLoop)
+    self.sound2=CreateSound(self,self:GetSonic().SoundLoop2)
     buttonsoundon=self:GetSonic().ButtonSoundOn
     buttonsoundoff=self:GetSonic().ButtonSoundOff
     holster=self:GetSonic().HolsterSound
 end)
 
 SWEP:AddHook("SonicChanged", "sound", function(self)
-    self.sound=CreateSound(self,self:GetSonic().SoundLoop)
+    self.sound1=CreateSound(self,self:GetSonic().SoundLoop)
+    self.sound2=CreateSound(self,self:GetSonic().SoundLoop2)
 end)
 
 SWEP:AddHook("OnRemove", "sound", function(self)
@@ -18,28 +20,46 @@ SWEP:AddHook("OnRemove", "sound", function(self)
 end)
 
 SWEP:AddHook("Holster", "sound", function(self)
-    if self.sound then self.sound:Stop() self:EmitSound(holster) end
+    if self.sound1 then self.sound1:Stop() self:EmitSound(holster) end
+    if self.sound2 then self.sound2:Stop() self:EmitSound(holster) end
 end)
 
 SWEP:AddHook("Think", "sound", function(self,keydown1,keydown2)
-    if keydown1 or keydown2 then
+    if keydown1 then
         if tobool(GetConVarNumber("sonic_sound"))==true then
             local diff=self.Owner:EyeAngles()-self.eyeangles
             if diff.p < 0 then diff.p=-diff.p end
             if diff.y < 0 then diff.y=-diff.y end
             local pitch=diff.p+diff.y*15
-            self.sound:ChangePitch(math.Clamp(pitch+100,100,150),0.1)
+            self.sound1:ChangePitch(math.Clamp(pitch+100,100,150),0.1)
             self.eyeangles=self.Owner:EyeAngles()
-            if not self.sound:IsPlaying() then
-                self.sound:Play()
+            if not self.sound1:IsPlaying() then
                 self:EmitSound(buttonsoundon)
+                self.sound1:Play()
             end
-        elseif self.sound and self.sound:IsPlaying() then
-            self.sound:Stop()
+        elseif self.sound1 and self.sound1:IsPlaying() then
             self:EmitSound(buttonsoundoff)
+            self.sound1:Stop()
         end
-    elseif self.sound and self.sound:IsPlaying() then
-        self.sound:Stop()
+    elseif keydown2 then
+        if tobool(GetConVarNumber("sonic_sound"))==true then
+            local diff=self.Owner:EyeAngles()-self.eyeangles
+            if diff.p < 0 then diff.p=-diff.p end
+            if diff.y < 0 then diff.y=-diff.y end
+            local pitch=diff.p+diff.y*15
+            self.sound2:ChangePitch(math.Clamp(pitch+100,100,150),0.1)
+            self.eyeangles=self.Owner:EyeAngles()
+            if not self.sound2:IsPlaying() then
+                self:EmitSound(buttonsoundon)
+                self.sound2:Play()
+            end
+        elseif self.sound2 and self.sound2:IsPlaying() then
+            self:EmitSound(buttonsoundoff)
+            self.sound2:Stop()
+        end
+    elseif (self.sound1 and self.sound1:IsPlaying()) or (self.sound2 and self.sound2:IsPlaying()) then
         self:EmitSound(buttonsoundoff)
+        self.sound1:Stop()
+        self.sound2:Stop()
     end
 end)
